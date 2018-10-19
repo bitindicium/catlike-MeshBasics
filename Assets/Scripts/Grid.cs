314ebb22526e5f621da11373701bcd5412ef280e
mboyle <m@mlboyle.com>
2018-10-18 23:15:57 -0400

    Complete.

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -11,10 +11,10 @@ public class Grid : MonoBehaviour
 
    private void Awake()
    {
-      StartCoroutine(Generate());
+      Generate();
    }
 
-   private IEnumerator Generate()
+   private void Generate()
    {
       var wait = new WaitForSeconds(0.05f);
 
@@ -22,13 +22,21 @@ public class Grid : MonoBehaviour
       _mesh.name = "Procedural Grid";
 
       _vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+      var uv = new Vector2[_vertices.Length];
+      var tangents = new Vector4[_vertices.Length];
+      var tangent = new Vector4(1f, 0f, 0f, -1f);
+
       for (int i = 0, y = 0; y <= ySize; y++) {
          for (int x = 0; x <= xSize; x++, i++) {
             _vertices[i] = new Vector3(x, y);
+            uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+            tangents[i] = tangent;
          }
       }
 
       _mesh.vertices = _vertices;
+      _mesh.uv = uv;
+      _mesh.tangents = tangents;
 
       var triangles = new int[xSize * ySize * 6];
       for (int ti = 0, vi = 0, y = 0; y < ySize; ++y, ++vi)
@@ -39,13 +47,11 @@ public class Grid : MonoBehaviour
             triangles[ti + 3] = triangles[ti + 2] = vi + 1;
             triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
             triangles[ti + 5] = vi + xSize + 2;
-
-            _mesh.triangles = triangles;
-            yield return wait;
          }
       }
 
       _mesh.triangles = triangles;
+      _mesh.RecalculateNormals();
    }
 
    private void OnDrawGizmos()
